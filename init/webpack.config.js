56e8793991bfa231148283039788648579f4be7b
cochkoch <cochkoch95@gmail.com>
2025-02-14 07:27:21 +0100

    Create node.js.yml

---
+++ init/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      },
+      include: path.join(__dirname, 'src')
+    }]
+  }
+};
